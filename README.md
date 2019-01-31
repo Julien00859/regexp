31d7095e7574af5f4ba3013150d724dc32863f92
Julien (juc) Castiaux <juc@odoo.com>
2019-01-31 14:24:46 +0100

    better script

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Regexp
 
-Just a toy repo to test stuff with regexp
+Just a toy repo to test stuff with regexp, expose a small
+script that mimic `GNU/grep` using the best regexp engine
+available.
+
+	usage: regexp [-h] [-q] [-f] regexp files [files ...]
+
+	positional arguments:
+	  regexp           Pattern to use
+	  files            Files to search
+
+	optional arguments:
+	  -h, --help       show this help message and exit
+	  -q, --quiet      Don't output lines found
+	  -f, --fullmatch  Match the pattern against a full line
--- __main__.py
+++ __main__.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from automatons import dcma, dca, da, nda
+from os.path import isfile
+from sys import exit as sys_exit
+from automatons import DCMA, DCA, DA, NDA
 
 parser = ArgumentParser()
-parser.add_argument("regexp")
-parser.add_argument("files")
-parser.add_argument("-q", dest="quiet", action="store_const", const=True, default=False)
-parser.add_argument("-f", dest="fullmatch", action="store_const", const=True, default=False)
+parser.add_argument("regexp", help="Pattern to use")
+parser.add_argument("files", nargs='+', help="Files to search")
+parser.add_argument("-q", "--quiet", dest="quiet", action="store_const", const=True, default=False,
+                    help="Don't output lines found")
+parser.add_argument("-f", "--fullmatch", dest="fullmatch", action="store_const", const=True, default=False,
+                    help="Match the pattern against a full line")
 args = parser.parse_args()
 
 if not args.fullmatch:
@@ -16,15 +20,22 @@ if not args.fullmatch:
     if not args.regexp.endswith("Σ*"):
         args.regexp = "%sΣ*" % args.regexp
 
-automaton = dcma.from_dca(dca.from_da(da.from_nda(nda.from_pattern(args.regexp))))
+automaton = NDA.from_pattern(args.regexp)
+try:
+    automaton = DA.from_nda(automaton)
+    automaton = DCA.from_da(automaton)
+    automaton = CDMA.from_dca(automaton)
+except NotImplementedError:
+    pass
+
 found = False
-for filepath in args.files:
+for filepath in filter(isfile, args.files):
     with open(filepath) as fd:
-        for line in fd.readlines():
+        for lineno, line in enumerate(fd.readlines()):
             if automaton.match(line):
                 found = True
                 if not args.quiet:
                     print(line)
 
-sys.exit(not found)
+sys_exit(not found)
 
